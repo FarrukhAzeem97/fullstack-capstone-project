24fa30b940bef3bbfbe29bf42713ebd8cd61730a
FarrukhAzeem97 <farrukhazeem257@gmail.com>
2024-11-03 05:50:33 -0500

    new changes

--- sentiment/index.js
+++ sentiment/index.js
@@ -37,7 +37,7 @@ app.post('/sentiment', async (req, res) => {
         let sentiment = "neutral";
 
         // Task 5: set sentiment to negative or positive based on score rules
-        if (analysisResult > 0) {
+        if (analysisResult > 0.33) {
             sentiment = "positive";
         } else if (analysisResult < 0) {
             sentiment = "negative";
